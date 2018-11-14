3a76c30d9b10883027a91141564435a068e84abf
leo <leo@163.com>
2018-11-14 19:04:25 +0800

    添加logback-spring.xml

---
+++ src/test/java/com/xinyan/sell/repository/ProductInfoRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.xinyan.sell.repository;
+
+import com.xinyan.sell.po.ProductInfo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductInfoRepositoryTest {
+    @Autowired
+    private ProductInfoRepository productInfoRepository;
+
+    @Test
+    public void findByProductStatus() {
+
+
+    }
+
+    @Test
+    public void findAll(){
+        List<ProductInfo> productInfoList = productInfoRepository.findAll();
+        System.out.println(productInfoList);
+
+    }
+}
